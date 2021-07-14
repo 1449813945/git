3ff028a443c0add19b85f127a2331d26a000fcff
long <long@163.com>
2021-07-15 00:55:30 +0800

    hot-fix commit1

--- 01-git/src/main/java/com/jxyyxy/App.java
+++ 01-git/src/main/java/com/jxyyxy/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println("Hello Git!");
+        System.out.println("正常合并分支hot-fix");
     }
 }
